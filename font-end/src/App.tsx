6a09aecbea40f221e4b851b95ec51f1933cf67ec
giorgiopeng <84867579@qq.com>
2020-03-13 21:41:55 +0800

    simple demo

--- font-end/src/App.tsx
+++ font-end/src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import L7 from './graphs/international/L7'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <L7/>
     </div>
   );
 }
