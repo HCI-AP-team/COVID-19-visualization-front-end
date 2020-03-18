5172f7646a60fedb9d0467caca6691bde38cf43b
giorgiopeng <84867579@qq.com>
2020-03-18 18:19:23 +0800

    dev: add control button, it is useless now

---
+++ src/directionButton/DirectionButton.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import { IconButton} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    margin: {
+      margin: theme.spacing(1),
+    },
+    downBtn:{
+      position:'fixed',
+      top:'50vh',
+      right:'1vw'
+    },
+    upBtn:{
+      position:'fixed',
+      top:'40vh',
+      right:'1vw'
+    }
+}));
+function DirectionButton() {
+    const classes = useStyles();
+    return (
+        <div>
+            {/* 上下移动页面的button */}
+            <IconButton className={classes.margin + ' ' + classes.upBtn}>
+                <ArrowUpwardIcon fontSize="large" />
+            </IconButton>
+            <IconButton className={classes.margin + ' ' + classes.downBtn}>
+                <ArrowDownwardIcon fontSize="large" />
+            </IconButton>
+        </div>
+    )
+}
+
+export default DirectionButton
