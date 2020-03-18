2422a7bf1edba6ee6e13ef02d6eea7a474a39058
giorgiopeng <84867579@qq.com>
2020-03-18 12:20:28 +0800

    dev: detail of each province

--- src/graphs/domestic/China.tsx
+++ src/graphs/domestic/China.tsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import red from '@material-ui/core/colors/red';
 import raphael from 'raphael';
 import chinaMap from '../../tempData/chinamapData'
 import areaData from '../../tempData/areaData'
 
 const useStyles = makeStyles({
-  root: { height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' },
-  labels: { width: '10vw', marginTop: '20vh', marginRight: '5vw' , border:'solid 2px black', borderRadius:'5px', padding:'2px'},
-  detail: { width: "30vw", height: '80vh', backgroundColor: 'white', border:"ridge 2px red", borderRadius:'5px', textAlign:'center', transition:'all 1s linear' },
+  root: {
+    height: '100vh',
+    width: '100vw',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  labels: {
+    width: '10vw',
+    marginTop: '20vh',
+    marginRight: '5vw',
+    border: 'solid 2px black',
+    borderRadius: '5px',
+    padding: '2px'
+  },
+  detail: {
+    width: "30vw",
+    height: '80vh',
+    backgroundColor: 'white',
+    border: "ridge 2px red",
+    borderRadius: '5px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    padding: '4px',
+    transition: 'all 1s linear',
+  },
 })
 
 interface dataForm {
@@ -57,6 +81,78 @@ function Label(props: any) {
   )
 }
 
+const DisplayDetail = (props: any) => {
+  interface DataStructure {
+    'locationId': number
+    'continentName': string
+    'continentEnglishName': string
+    'countryName': string
+    'countryEnglishName': string
+    'provinceName': string
+    'provinceShortName': string
+    'provinceEnglishName': string
+    'currentConfirmedCount': number
+    'confirmedCount': number
+    'suspectedCount': number
+    'curedCount': number
+    'deadCount': number
+    'cities': object[]
+    'comment': string
+    'updateTime': number
+  }
+  const provinceName = props.currentChoose;
+
+  //将时间戳转化成当前时间
+  function getTime(time:any){
+    let date = new Date(time);
+    let Y = date.getFullYear() + '-';
+    let M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    let D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' ';
+    let h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+    let m = (date.getMinutes() <10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+    let s = (date.getSeconds() <10 ? '0' + date.getSeconds() : date.getSeconds());
+    return Y+M+D+h+m+s;
+  }
+
+  //过滤出来某个省的数据
+  const [temp, setTemp]: [DataStructure[] | undefined, Function] = useState();
+  useEffect(() => {
+    setTemp(areaData.results.filter((value) => value.countryEnglishName === "China" && value.provinceShortName === provinceName) as DataStructure[]);
+  }, [provinceName])
+  useEffect(() => {
+    console.log(temp)
+  }, [temp])
+  return (
+    <div>
+      {temp ?
+        temp[0] ? Object.keys(temp[0]).map((key) => {
+          if (key !== 'cities' && key !== "locationId"&&key!=='comment'&&key!=="updateTime") {
+            return <span key={key} style={{ display: 'flex', padding: '2px', justifyContent: 'space-between'}}>
+              <p>{key + ' : '}</p>
+              <p>{temp ? (temp[0] as any)[key] : ''}</p>
+            </span>
+          }
+          else{
+            if(key==="updateTime")
+              return <span key={key} style={{ display: 'flex', padding: '2px', justifyContent: 'space-between'}}>
+                      <p>{key + ' : '}</p>
+                      <p>{temp ? getTime((temp[0] as any)[key]) : ''}</p>
+                    </span>
+            ;
+            else
+              return undefined
+          }
+          
+        }
+        )
+          :
+          <p style={{textAlign:'center',fontSize:'30px'}}>点击各省可以查看该省的详细情况</p>
+        :
+        <p>点击各省可以查看该省的详细情况</p>
+      }
+    </div>
+  )
+}
 
 function China() {
   const [currentChoose, setCurrentChoose] = useState('');
@@ -92,12 +188,12 @@ function China() {
     const map = raphael("map", chinaMap.dimension.width, chinaMap.dimension.height);
 
     //数据清洗
-    let data: dataForm[] = areaData.results.map((el: any) => {
-      if (el.countryName == '中国') {
+    let data: dataForm[] = areaData.results.map((el: any): {} | undefined => {
+      if (el.countryName === '中国') {
         return { 省份: el.provinceShortName, value: el.curedCount }
       }
       else
-        return;
+        return undefined;
     }) as dataForm[];
     data = data.filter((el: any) => el !== undefined)
     data = data.sort((a: any, b: any) => - b.value + a.value)
@@ -124,22 +220,22 @@ function China() {
 
       //每个省份图形的点击事件
       tempPath.click(
-        function (this: typeof tempPath) {
-          setCurrentChoose(value.name+'的详情信息');
+        () => {
+          setCurrentChoose(value.name);
         }
       )
 
       //每个省份都添加事件来确认鼠标位置
       tempPath.mouseover(
-        ()=>{
-          tempPath.attr("fill","#00e5ff")
+        () => {
+          tempPath.attr("fill", "#00e5ff")
         }
       )
 
       //每个省份都添加事件来确认鼠标位置
       tempPath.mouseout(
-        ()=>{
-          tempPath.attr("fill",color)
+        () => {
+          tempPath.attr("fill", color)
         }
       )
 
@@ -156,24 +252,30 @@ function China() {
 
       //每个省份都添加事件来确认鼠标位置
       text.mouseover(
-        ()=>{
-          tempPath.attr("fill","#00e5ff")
+        () => {
+          tempPath.attr("fill", "#00e5ff")
         }
       )
 
       //每个省份都添加事件来确认鼠标位置
       text.mouseout(
-        ()=>{
-          tempPath.attr("fill",color)
+        () => {
+          tempPath.attr("fill", color)
+        }
+      )
+
+      //每个省份图形的点击事件
+      text.click(
+        () => {
+          setCurrentChoose(value.name);
         }
       )
     })
   }
-
   const classes = useStyles()
   useEffect(() => {
     createChinaMap();
-  },[])
+  }, [])
   return (
     <div className={classes.root}>
       <div id="map" ></div>
@@ -181,7 +283,9 @@ function China() {
         <strong>确诊人数</strong>
         {labels.map((el: any, index: number) => <Label key={index} color={el.color} value={el.value} />)}
       </div>
-      <div id="detail"  className={classes.detail}>{currentChoose?currentChoose:'点击各省可以查看该省的详细情况'}</div>
+      <div id="detail" className={classes.detail}>
+        <DisplayDetail currentChoose={currentChoose} />
+      </div>
     </div>
   )
 }
