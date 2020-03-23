e4a79a52539cb9bc45131f5260df6c659e002d5a
giorgiopeng <84867579@qq.com>
2020-03-23 22:29:50 +0800

    update: new version

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles({
+    root: {
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection:'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        '& p': {
+            fontSize: '50px',
+            color: 'white',
+            textAlign: 'center',
+            width: '70vw'
+        }
+    },
+    hint:{
+        border:'2px solid #304ffe',
+        borderRadius:'5px',
+        padding:'15px',
+        textAlign:'center',
+        color:'white',
+        transition:'1s linear all',
+        fontWeight:'bold'
+    }
+})
 function Homepage() {
+    const displayString = "Novel coronavirus pneumonia is a novel coronavirus pneumonia visualization project. No one can avoid this epidemic. The epidemic in China has been gradually improved, and the epidemic is spreading abroad. We hope that this project will help people understand the epidemic situation more profoundly."
+    const [str, setStr] = useState('')
+    const [showHint, setShowHint] = useState(false)
+    const classes = useStyles();
+    useEffect(() => {
+        let i = 1;
+        let st = setInterval(() => {
+            setStr(displayString.substr(0, i++))//逐步显示文字
+            if (i === displayString.length + 1) {
+                clearInterval(st);//清除间歇调用
+                setShowHint(true);//显示提示框
+            }
+        }, 50);
+    }, [])
     return (
-        <div style={{width:'100vw',height:'100vh',fontSize:'50px',lineHeight:'100vh',textAlign:'center'}}>
-            This is the homepage
+        <div className={classes.root}>
+            <p>{str}</p>
+            <div className={classes.hint} style={{opacity:showHint?1:0}}>Glide and learn more</div>
         </div>
     )
 }
