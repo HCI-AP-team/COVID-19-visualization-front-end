0ec634c504896a34eaa6fc64a79fba014f152ebb
giorgiopeng <84867579@qq.com>
2020-03-26 15:13:14 +0800

    fear: new style

--- src/components/DirectionButton.tsx
+++ src/components/DirectionButton.tsx
@@ -1,38 +1,69 @@
-import React from 'react'
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import { IconButton } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import SpeedDial from '@material-ui/lab/SpeedDial';
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
+import LocationCity from '@material-ui/icons/LocationCity';
+import VpnLock from '@material-ui/icons/VpnLock';
+import People from '@material-ui/icons/People';
+import Public from '@material-ui/icons/Public';
+import SwapCalls from '@material-ui/icons/SwapCalls'
+import Home from '@material-ui/icons/Home';
+import SwapVert from '@material-ui/icons/SwapVert';
 
-const useStyles = makeStyles(theme => ({
-  margin: {
-    margin: theme.spacing(1),
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  downBtn: {
-    position: 'fixed',
-    top: '50vh',
-    right: '1vw'
-  },
-  upBtn: {
-    position: 'fixed',
-    top: '40vh',
-    right: '1vw'
-  }
-}));
-function DirectionButton() {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      position: 'fixed',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+      height: 380,
+      transform: 'translateZ(0px)',
+      flexGrow: 1,
+      zIndex: 999
+    }
+  }),
+);
+
+const actions = [
+  { icon: <LocationCity onClick={() => { window.scrollTo({ top: window.innerHeight * 4, behavior: 'smooth' }) }} />, name: 'City' },
+  { icon: <People onClick={() => { window.scrollTo({ top: window.innerHeight * 3, behavior: 'smooth' }) }} />, name: 'Province' },
+  { icon: <VpnLock onClick={() => { window.scrollTo({ top: window.innerHeight * 2, behavior: 'smooth' }) }} />, name: 'China' },
+  { icon: <Public onClick={() => { window.scrollTo({ top: window.innerHeight, behavior: 'smooth' }) }} />, name: 'International' },
+  { icon: <Home onClick={() => { window.scrollTo({ top: 0, behavior: 'smooth' }) }} />, name: 'Homepage' },
+];
+
+export default function OpenIconSpeedDial() {
   const classes = useStyles();
-  return (
-    <div>
-      {/* 上下移动页面的button */}
-      <IconButton className={classes.margin + ' ' + classes.upBtn}>
-        <ArrowUpwardIcon fontSize="large" />
-      </IconButton>
-      <IconButton className={classes.margin + ' ' + classes.downBtn}>
-        <ArrowDownwardIcon fontSize="large" />
-      </IconButton>
-    </div>
-  )
-}
+  const [open, setOpen] = React.useState(false);
 
-export default DirectionButton
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <SpeedDial
+        ariaLabel="SpeedDial openIcon example"
+        icon={<SpeedDialIcon icon={<SwapVert/>} openIcon={<SwapCalls />} />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {actions.map(action => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            onClick={handleClose}
+          />
+        ))}
+      </SpeedDial>
+    </div>
+  );
+}
