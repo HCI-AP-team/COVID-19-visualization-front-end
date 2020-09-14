f70e6cb78446f0413b715ebc137d748f62d91ea0
Giorgio <84867579@qq.com>
2020-09-14 14:05:21 +0800

    Add files via upload

---
+++ precache-manifest.281a5164ffd0f2352e2b2691f7d67ba6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa29649fdc66f7cbe3f885b1c9aaf980",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7487facef65f7fc52326",
+    "url": "./static/css/main.c64fdf6a.chunk.css"
+  },
+  {
+    "revision": "d75b371e2aefb603a7c0",
+    "url": "./static/js/2.58b39e80.chunk.js"
+  },
+  {
+    "revision": "c2191ba7d3fd7c1834d6eba0ce15acdc",
+    "url": "./static/js/2.58b39e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aed13a74845bd6eedcc",
+    "url": "./static/js/3.128d64a0.chunk.js"
+  },
+  {
+    "revision": "a5e6b004202c0344230a",
+    "url": "./static/js/4.25c0235b.chunk.js"
+  },
+  {
+    "revision": "7487facef65f7fc52326",
+    "url": "./static/js/main.8badddb6.chunk.js"
+  },
+  {
+    "revision": "4f899a9c9b0283e7f4d8",
+    "url": "./static/js/runtime-main.8193c64b.js"
+  }
+]);
